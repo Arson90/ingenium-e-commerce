3305659b2f68231c9cd20a52606b3a69100bc387
Arek <arek1990cda@gmail.com>
2022-11-09 19:47:15 +0100

    create customer entity, dto and view

---
+++ src/main/java/com/ingenium/ingeniumecommerce/customer/Customer.java
@@ -0,0 +1,24 @@
+package com.ingenium.ingeniumecommerce.customer;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+}

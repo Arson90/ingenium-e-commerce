b972e87c32a96307e4fcb8ca95a08a2aca89eaaf
Arkadiusz Krolikowski <arkadiusz_krolikowski@epam.com>
2022-11-09 19:15:11 +0100

    project initialization

---
+++ src/test/java/com/ingenium/ingeniumecommerce/IngeniumECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingenium.ingeniumecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngeniumECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

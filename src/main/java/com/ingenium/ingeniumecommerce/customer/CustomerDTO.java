8fae7e69d47a885781fa00f1596d9e2d23fa39bd
Arek <arek1990cda@gmail.com>
2022-11-09 21:33:12 +0100

    create customer entity, dto and view

---
+++ src/main/java/com/ingenium/ingeniumecommerce/customer/CustomerDTO.java
@@ -0,0 +1,11 @@
+package com.ingenium.ingeniumecommerce.customer;
+
+import lombok.Getter;
+
+@Getter
+public class CustomerDTO {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+}

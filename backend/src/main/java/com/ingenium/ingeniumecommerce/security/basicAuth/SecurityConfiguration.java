25c86e7eddd95206e9706f56149fd6cfe1b3cdf6
Arek <arek1990cda@gmail.com>
2022-12-06 09:40:11 +0100

    implement main page to show products

--- backend/src/main/java/com/ingenium/ingeniumecommerce/security/basicAuth/SecurityConfiguration.java
+++ backend/src/main/java/com/ingenium/ingeniumecommerce/security/basicAuth/SecurityConfiguration.java
@@ -10,6 +10,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -23,10 +28,11 @@ public class SecurityConfiguration{
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().authorizeRequests()
-                .antMatchers("/ingenium/products").permitAll()
+                .antMatchers("/ingenium/**").permitAll()
+//                .antMatchers("/ingenium/products").permitAll()
                 .antMatchers(HttpMethod.POST,"/ingenium/register").permitAll()
-                .antMatchers("/ingenium/carts/**").permitAll()
-                .anyRequest().authenticated()
+//                .antMatchers("/ingenium/carts/**").permitAll()
+//                .anyRequest().authenticated()
                 .and().formLogin();
         return http.build();
     }
@@ -34,5 +40,21 @@ public class SecurityConfiguration{
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+                "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+                "Access-Control-Request-Method", "Access-Control-Request-Headers"));
+        corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
+                "Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(urlBasedCorsConfigurationSource);
     }
 }

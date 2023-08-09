de924be9923cedc1e9346a7a71efa5e9c5e37e4d
Arek <arek1990cda@gmail.com>
2023-08-09 18:26:20 +0200

    remove unnecessary components

--- backend/src/main/java/com/ingenium/ingeniumecommerce/address/AddressCommandRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ingenium.ingeniumecommerce.address;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AddressCommandRepository extends JpaRepository<Address, Long> {
-}

3d51eebbc58c3892542fe91d07b8cf2a13229987
Arek <arek1990cda@gmail.com>
2023-08-11 19:32:40 +0200

    change API params, add getter and setter for jwt, change implement in login

--- backend/src/main/java/com/ingenium/ingeniumecommerce/security/auth/AuthenticationController.java
+++ backend/src/main/java/com/ingenium/ingeniumecommerce/security/auth/AuthenticationController.java
@@ -1,5 +1,6 @@
 package com.ingenium.ingeniumecommerce.security.auth;
 
+import com.ingenium.ingeniumecommerce.constant.RestApiUrl;
 import com.ingenium.ingeniumecommerce.user.UserRequestDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -9,20 +10,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/ingenium")
 @RequiredArgsConstructor
 public class AuthenticationController {
-    private final AuthenticationService authenticationService;
+    private final AuthenticationServiceImpl authenticationServiceImpl;
 
-    @PostMapping("/register")
+    @PostMapping(RestApiUrl.Page.REGISTER)
     ResponseEntity<Void> register(@RequestBody final UserRequestDTO userRequestDTO) {
-        authenticationService.register(userRequestDTO);
+        authenticationServiceImpl.register(userRequestDTO);
         return ResponseEntity.ok().build();
     }
 
-    @PostMapping("/authenticate")
+    @PostMapping(RestApiUrl.Page.LOGIN)
     ResponseEntity<AuthenticationResponse> authenticate(@RequestBody final AuthenticationRequest request) {
-       final AuthenticationResponse authenticationResponse = authenticationService.authenticate(request);
+       final AuthenticationResponse authenticationResponse = authenticationServiceImpl.authenticate(request);
        return ResponseEntity.ok(authenticationResponse);
     }
 }

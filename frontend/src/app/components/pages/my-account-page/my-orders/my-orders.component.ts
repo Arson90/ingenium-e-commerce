283f8f29b7260ab90dd09398793489f18f1f35a0
Arek <arek1990cda@gmail.com>
2023-09-26 14:42:38 +0200

    add my-orders-comp to get my orders

--- frontend/src/app/components/pages/my-account-page/my-orders/my-orders.component.ts
+++ frontend/src/app/components/pages/my-account-page/my-orders/my-orders.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { OrderData } from "../../../../types/order";
+import { OrderService } from "../../../../services/order/order.service";
 
 @Component({
   selector: 'app-my-orders',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-orders.component.css']
 })
 export class MyOrdersComponent implements OnInit {
+  orderData: OrderData[];
 
-  constructor() { }
+  constructor(private orderService: OrderService) { }
 
   ngOnInit(): void {
+    this.getMyOrderData();
   }
 
+  getMyOrderData() {
+    this.orderService.getMyOrders()
+      .subscribe({
+        next: (response: OrderData[]) => {
+          this.orderData = {...response}
+      },
+        error: (error) => console.error(error.error, ' ', error.status),
+        complete: () => console.info('Order data retrieved')
+      })
+  }
 }

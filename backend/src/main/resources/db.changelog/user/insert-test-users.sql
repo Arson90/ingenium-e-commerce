e38400095ffb442d4d58e3e06ad89b87d6cb48e7
Arson <arek1990cda@gmail.com>
2023-12-13 10:50:04 +0100

    add actions, module, reducer, selectors, type, cart component

--- backend/src/main/resources/db.changelog/user/insert-test-users.sql
+++ backend/src/main/resources/db.changelog/user/insert-test-users.sql
@@ -1,3 +1,4 @@
 --liquibase formatted sql
 --changeset arek:8 runOnChange:true
 insert into users (id, username, password, role, customer_id) values (1, 'admin', '$2a$12$EE52zrXeAfN2jJ4bqJ4knO0aGcHbJvr/NMSIDQMeRqCgnmdHc58wG','ROLE_ADMIN', null);
+insert into users (id, username, password, role, customer_id) values (2, 'test', '$2a$10$VZDDMdwVuINRzpesYD/CGe3W4o8VrXHSrcytmsP.pOVo46GfSV1ZW','ROLE_USER', null);
